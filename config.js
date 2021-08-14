a4fd08e0127bc0d34a74cc5b3b7f435d5ed0cdeb
yinno16 <88919623+yinno16@users.noreply.github.com>
2021-08-14 12:17:20 +0700

    Update config.js

--- config.js
+++ config.js
@@ -11,9 +11,9 @@ const CONFIG = {
     btnNo: 'Không, Anh trai à :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
-    reply: 'Anh chỉ cần biết là em yêu anh rất nhiều thôi:>>>>',
+    reply: 'Anh chỉ cần biết là em yêu anh rất nhiều thôi:3',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    messDesc: 'Tối nay 7h nhắn tin cho anh nha.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://www.facebook.com/phamduckman' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
