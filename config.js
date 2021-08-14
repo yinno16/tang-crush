c72493f0a6abb3be11b68cc5990e1b17d032c59d
yinno16 <88919623+yinno16@users.noreply.github.com>
2021-08-14 12:13:11 +0700

    Update config.js

--- config.js
+++ config.js
@@ -11,7 +11,7 @@ const CONFIG = {
     btnNo: 'Không, Anh trai à :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu anh rất là nhiều',
+    reply: 'Anh chỉ cần biết là em yêu anh rất nhiều thôi:>>>>',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
     messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
     btnAccept: 'Okiiiii lun <3',
